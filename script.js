3c9ee840cb803b17b651ac75c209b3436a473532
Vennesavee <126670814+Vennesavee@users.noreply.github.com>
2023-07-10 18:20:17 +0300

    Added an eventListener on the button

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function getWorkoutPlan(plan) {
+  alert("This is your workout plan")
+}
+
+
+
+let button = document.querySelector(".click-button");
+button.addEventListener("click",getWorkoutPlan);
